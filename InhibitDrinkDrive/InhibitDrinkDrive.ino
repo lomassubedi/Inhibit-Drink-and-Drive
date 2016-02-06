65ee56483d2915d2d57bee533b2189d67e93e3ab
lomassubedi <subedilomas@gmail.com>
2016-02-06 11:52:08 +0545

    Corrected latitude and longitude conversion.

--- InhibitDrinkDrive/InhibitDrinkDrive.ino
+++ InhibitDrinkDrive/InhibitDrinkDrive.ino
@@ -20,9 +20,6 @@ String myData;
 // String storing Formatted Address of the vehicle
 String vehicleLocation;
 
-double latitude;
-double longitude;
-
 // ---- GPIO for Switching ------
 char alcoholSensor = A0;
 const int relayPin = 8;
@@ -35,7 +32,15 @@ int localHr, localMn, localSec;
 
 gpsSentenceInfoStruct info;
 
-char buff[256];
+char buff[256], secBuff[256];
+struct GPSDetails {
+  float latitude;
+  float longitude;
+  int hour, minute, second, tmp, fix, num; 
+  char latitude_dir, longitude_dir;
+};
+struct GPSDetails MyGPSPos;
+float finalLng, finalLtd, tmpLng, tmpLtd;
 
 //---------- Function to send SMS --------
 void sendSMS(){
@@ -67,9 +72,9 @@ void sendSMS(){
     LSMS.print(vehicleLocation);
     LSMS.println('.');
     LSMS.print("Corrosponding geo-location i.e. lat, lng =  ");
-    LSMS.print(latitude, 6);
+    LSMS.print(finalLtd, 6);
     LSMS.print(',');
-    LSMS.print(longitude, 6);
+    LSMS.print(finalLng, 6);
     LSMS.println(". ");    
     LSMS.endSMS();    
     if(LSMS.endSMS())
@@ -82,27 +87,31 @@ void sendSMS(){
     }     
  }
  //-------- End of function sending SMS -------
- 
-static unsigned char getComma(unsigned char num,const char *str)
-{
+//----------------------------------------------------------------------
+//!\brief  return position of the comma number 'num' in the char array 'str'
+//!\return  char
+//----------------------------------------------------------------------
+static unsigned char getComma(unsigned char num,const char *str){
   unsigned char i,j = 0;
   int len=strlen(str);
-  for(i = 0;i < len;i ++)
-  {
-     if(str[i] == ',')
+  for(i = 0;i < len;i ++){
+    if(str[i] == ',')
       j++;
-     if(j == num)
+    if(j == num)
       return i + 1; 
-  }
+    }
   return 0; 
 }
 
-static double getDoubleNumber(const char *s)
-{
+//----------------------------------------------------------------------
+//!\brief convert char buffer to float
+//!\return  float
+//----------------------------------------------------------------------
+static float getFloatNumber(const char *s){
   char buf[10];
   unsigned char i;
-  double rev;
-  
+  float rev;
+
   i=getComma(1, s);
   i = i - 1;
   strncpy(buf, s, i);
@@ -111,11 +120,15 @@ static double getDoubleNumber(const char *s)
   return rev; 
 }
 
-static double getIntNumber(const char *s)
-{
+//----------------------------------------------------------------------
+//!\brief convert char buffer to int
+//!\return  float
+//----------------------------------------------------------------------
+static float getIntNumber(const char *s){
   char buf[10];
   unsigned char i;
-  double rev;  
+  float rev;
+
   i=getComma(1, s);
   i = i - 1;
   strncpy(buf, s, i);
@@ -124,19 +137,52 @@ static double getIntNumber(const char *s)
   return rev; 
 }
 
-void parseGPGGA(const char* GPGGAstr)
-{  
-  int tmp, num ;
-  int hour, minute, second;
-  if(GPGGAstr[0] == '$')
-  {
+//----------------------------------------------------------------------
+//!\brief Parse GPS NMEA buffer for extracting data
+//!\return  -
+//----------------------------------------------------------------------
+void parseGPGGA(const char* GPGGAstr){
+  if(GPGGAstr[0] == '$'){
+    int tmp;
     tmp = getComma(1, GPGGAstr);
-    hour     = (GPGGAstr[tmp + 0] - '0') * 10 + (GPGGAstr[tmp + 1] - '0');
-    minute   = (GPGGAstr[tmp + 2] - '0') * 10 + (GPGGAstr[tmp + 3] - '0');
-    second    = (GPGGAstr[tmp + 4] - '0') * 10 + (GPGGAstr[tmp + 5] - '0');         
+    MyGPSPos.hour     = (GPGGAstr[tmp + 0] - '0') * 10 + (GPGGAstr[tmp + 1] - '0');
+    MyGPSPos.minute   = (GPGGAstr[tmp + 2] - '0') * 10 + (GPGGAstr[tmp + 3] - '0');
+    MyGPSPos.second    = (GPGGAstr[tmp + 4] - '0') * 10 + (GPGGAstr[tmp + 5] - '0');
+
+    //get time
+    sprintf(buff, "UTC timer %2d-%2d-%2d", MyGPSPos.hour, MyGPSPos.minute, MyGPSPos.second);
+    Serial.print(buff);
+    //get lat/lon coordinates
+    float latitudetmp;
+    float longitudetmp;
+    tmp = getComma(2, GPGGAstr);
+    latitudetmp = getFloatNumber(&GPGGAstr[tmp]);
+    tmp = getComma(4, GPGGAstr);
+    longitudetmp = getFloatNumber(&GPGGAstr[tmp]);
+    // need to convert format
+    convertCoords(latitudetmp, longitudetmp, MyGPSPos.latitude, MyGPSPos.longitude);
+    //get lat/lon direction
+    tmp = getComma(3, GPGGAstr);
+    MyGPSPos.latitude_dir = (GPGGAstr[tmp]);
+    tmp = getComma(5, GPGGAstr);    
+    MyGPSPos.longitude_dir = (GPGGAstr[tmp]);
     
+    //sprintf(buff, "latitude = %10.4f-%c, longitude = %10.4f-%c", MyGPSPos.latitude, MyGPSPos.latitude_dir, MyGPSPos.longitude, MyGPSPos.longitude_dir);
+    //Serial.println(buff); 
+    
+    //get GPS fix quality
+    tmp = getComma(6, GPGGAstr);
+    MyGPSPos.fix = getIntNumber(&GPGGAstr[tmp]);    
+    sprintf(buff, "  -  GPS fix quality = %d", MyGPSPos.fix);
+    Serial.print(buff);   
+    //get satellites in view
+    tmp = getComma(7, GPGGAstr);
+    MyGPSPos.num = getIntNumber(&GPGGAstr[tmp]);    
+    sprintf(buff, "  -  %d satellites", MyGPSPos.num);
+    Serial.println(buff);
+
     // Convert UTC to NPT (Nepalese Local Time +5:45)    
-    localSecs = hour * 3600 + minute * 60 + second + 20700; // 5hr+45min = 20700sec
+    localSecs = MyGPSPos.hour * 3600 + MyGPSPos.minute * 60 + MyGPSPos.second + 20700; // 5hr+45min = 20700sec
     if(localSecs > 86399) {
       localSecs = localSecs - 86400;
     }
@@ -144,25 +190,36 @@ void parseGPGGA(const char* GPGGAstr)
     tmpVal = localSecs % 3600;
     localMn = tmpVal / 60;
     localSec = tmpVal % 60;
-           
-    tmp = getComma(2, GPGGAstr);
-    latitude = getDoubleNumber(&GPGGAstr[tmp]);
-    latitude /= 100;
-    tmp = getComma(4, GPGGAstr);
-    longitude = getDoubleNumber(&GPGGAstr[tmp]);
-    longitude /= 100;
+    sprintf(buff, "Local time %2d:%2d:%2d", localHr, localMn, localSec);
+    Serial.println(buff);    
     
-    tmp = getComma(7, GPGGAstr);
-    num = getIntNumber(&GPGGAstr[tmp]);    
-//    sprintf(buff, "satellites number = %d", num);
-    Serial.println(buff); 
+  //Convert to google map compatible address
+  float finalLng, finalLtd;
+  convertCoords(latitudetmp, longitudetmp, finalLtd, finalLng);
+  sprintf(buff, "Latitude : %.6f, Longitude: %.6f", finalLtd, finalLng);
+  Serial.println(buff);
   }
-  else
-  {
-    Serial.println("Not get data"); 
+  else{
+    Serial.println("No GPS data"); 
   }
 }
-void readJSONAddress(double latitude, double longitude) {
+
+//----------------------------------------------------------------------
+//!\brief Convert GPGGA coordinates (degrees-mins-secs) to true decimal-degrees
+//!\return  -
+//----------------------------------------------------------------------
+void convertCoords(float latitude, float longitude, float &lat_return, float &lon_return){
+  int lat_deg_int = int(latitude/100);    //extract the first 2 chars to get the latitudinal degrees
+  int lon_deg_int = int(longitude/100);   //extract first 3 chars to get the longitudinal degrees
+    // must now take remainder/60
+    //this is to convert from degrees-mins-secs to decimal degrees
+    // so the coordinates are "google mappable"
+    float latitude_float = latitude - lat_deg_int * 100;    //remove the degrees part of the coordinates - so we are left with only minutes-seconds part of the coordinates
+    float longitude_float = longitude - lon_deg_int * 100;     
+    lat_return = lat_deg_int + latitude_float / 60 ;      //add back on the degrees part, so it is decimal degrees
+    lon_return = lon_deg_int + longitude_float / 60 ;
+}
+void readJSONAddress(float latitude, float longitude) {
   //Host for Google Map API
   char host[] = "maps.googleapis.com";
   // Constant path for JSON 
@@ -272,9 +329,9 @@ void loop() {
   LGPS.getData(&info);  
   parseGPGGA((const char*)info.GPGGA); 
   Serial.print("Latitude: ");
-  Serial.print(latitude, 6);
+  Serial.print(finalLtd, 6);
   Serial.print(", Longitude: ");
-  Serial.println(longitude, 6);
+  Serial.println(finalLng, 6);
   //------------------------------------------    
   delay(500);  
   // A constant value is compared from practical measurement.
@@ -298,7 +355,7 @@ void loop() {
       delay(2000);
     }  
     Serial.println("Reading JSON file from Google Map API. It may take a minute...");
-    readJSONAddress(latitude,longitude);  
+    readJSONAddress(finalLtd,finalLng);  
     
     DynamicJsonBuffer jsonBuffer;    
     Serial.println("Reading JSON file from Google Map API Done.");
